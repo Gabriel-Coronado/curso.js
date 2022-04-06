749e5eaab63137325f437aafeb71bb751d4284f8
Gabriel Coronado Pacheco <gabrielcp14@outlook.com>
2022-04-06 11:47:59 -0300

    Update Condicional.js

--- meu-projeto2/src/components/Condicional.js
+++ meu-projeto2/src/components/Condicional.js
@@ -8,6 +8,7 @@ function Condicional(){
             setUserPassword(password)
             console.log(UserEmail)
         }
+        
         function limparEmail(){
             setUserEmail('')
             setUserPassword('')
@@ -18,26 +19,36 @@ function Condicional(){
         const [UserEmail, setUserEmail]=useState()
         const [UserPassword, setUserPassword]=useState()
 
-
-    
+        
         return(
             <div>
                 <h2>Cadastre o seu e-mail</h2>
                 <form onSubmit={enviarEmail}>
                     <input type='e-mail' placeholder='Cadastre seu e-mail...' onChange={(e)=>setEmail(e.target.value)}/>
                     <br></br><br></br>
-                    <input type='password' id='password' name='password' placeholder='Cadastre sua senha...' onChange={(e)=>setPassword(e.target.value)}/> 
+                    <input type='password' id='password2' name='password' placeholder='Cadastre sua senha...' onChange={(e)=>setPassword(e.target.value)}/> 
                     <br></br>     <br></br>
                     <button type='submit' onClick={enviarEmail}>Cadastrar</button>              
                 </form>
-                {UserEmail||UserPassword && (
+                
+                {UserEmail && (
                     <div>
                         <p>Usuário {UserEmail}</p>
-                        <button onClick={limparEmail}>Limpar Email</button>
+                        
                     </div>
                 )}
-            </div>
-        )
+                {UserPassword &&(
+                    <div>
+                        <p>Senha cadastrada</p>
+                        <button onClick={limparEmail}>Limpar informações</button>
+                       
+                    </div>
+                )
+               
+                }
+            
+            </div> 
+        )  
 
 }
 export default Condicional
